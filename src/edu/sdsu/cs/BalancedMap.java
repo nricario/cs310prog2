15951607b4d8af8cab992c92dd879e63fc0936b0
Nick <nick@ricario.com>
2018-10-31 14:20:54 -0700

    first commit with all neccesary files

---
+++ src/edu/sdsu/cs/BalancedMap.java
@@ -0,0 +1,5 @@
+package edu.sdsu.cs;
+
+public class BalancedMap {
+
+}
---
+++ src/edu/sdsu/cs/UnbalancedMap.java
@@ -0,0 +1,74 @@
+package edu.sdsu.cs;
+
+import java.util.TreeMap;
+import java.util.LinkedList;
+
+public class UnbalancedMap implements IMap {
+	TreeMap t;
+
+
+	@Override
+	public boolean contains(Comparable key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Comparable key, Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object delete(Comparable key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getValue(Comparable key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable getKey(Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable getKeys(Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterable keyset() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable values() {
+		// TODO Auto-generated method stub
+		return null;
+	}}
