19068dc3ccde25ae9fbde1ae959315943f261208
hainguyen2498 <44654620+hainguyen2498@users.noreply.github.com>
2018-11-02 20:18:24 -0700

    Final changes

--- src/edu/sdsu/cs/BalancedMap.java
+++ src/edu/sdsu/cs/BalancedMap.java
@@ -2,80 +2,91 @@ package edu.sdsu.cs;
 
 import edu.sdsu.cs.datastructures.IMap;
 import java.util.TreeMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.ArrayList;
 
-public class BalancedMap<K, V> implements IMap {
-	TreeMap<K, V> map;
-	public BalancedMap() {
-		map = new TreeMap<K, V>();
-	}
+public class BalancedMap<K extends Comparable<K>, V> implements IMap {
+    TreeMap<K, V> map;
 
-	@Override
-	public boolean contains(Comparable key) {
-		return map.containsKey(key);
-	}
+    public BalancedMap() {
+        map = new TreeMap<K, V>();
+    }
 
-	@Override
-	public boolean add(Comparable key, Object value) {
-		return map.put(key, value);
-	}
+    public BalancedMap(IMap<K, V> imap) {
+        map = new TreeMap<K, V>();
+        for (K key : imap.keyset()) {
+            map.put(key, imap.getValue(key));
+        }
+    }
 
-	@Override
-	public Object delete(Comparable key) {
-		return map.remove(key);
-	}
+    @Override
+    public boolean contains(Comparable key) {
+        return map.containsKey(key);
+    }
 
-	@Override
-	public Object getValue(Comparable key) {
-		return map.get(key);
-	}
+    @Override
+    public boolean add(Comparable key, Object value) {
+        if (map.containsKey(key)) {
+            return false;
+        }
 
-	@Override
-	public Comparable<K> getKey(Object value) {
-		Iterator<K> keyIter = map.keySet().iterator();
-		Iterator<V> valueIter = map.values().iterator();
-		while (keyIter.hasNext()) {
-			Object tempV = valueIter.next();
-			if (tempV.equals(value)) {
-				Comparable<K> tempV2 = (Comparable<K>) tempV;
-				return tempV2;
-			}
-		}
-		return (Comparable<K>) map.get(value);
-	}
-		
+        V ret = map.put((K) key, (V) value);
+        return true;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Iterable<K> getKeys(Object value) {
-		return (Iterable<K>) map.get(value);
-	}
+    @Override
+    public Object delete(Comparable key) {
+        return map.remove(key);
+    }
 
-	@Override
-	public int size() {
-		return map.size();
-	}
+    @Override
+    public Object getValue(Comparable key) {
+        return map.get(key);
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return map.isEmpty();
-	}
+    @Override
+    public Comparable<K> getKey(Object value) {
+        for (K key : map.keySet()) {
+            if (map.get(key).equals(value)) {
+                return key;
+            }
+        }
 
-	@Override
-	public void clear() {
-		map.clear();
-	}
+        return null;
+    }
 
-	@Override
-	public Iterable<K> keyset() {
-		return map.keySet();
-	}
+    @Override
+    public Iterable<K> getKeys(Object value) {
+        ArrayList<K> keys = new ArrayList<K>();
+        for (K key : map.keySet()) {
+            if (map.get(key).equals(value)) {
+                keys.add(key);
+            }
+        }
+        return keys;
+    }
 
-	@Override
-	public Iterable<V> values() {
-		return map.values();
-	}
+    @Override
+    public int size() {
+        return map.size();
+    }
 
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Iterable<K> keyset() {
+        return map.keySet();
+    }
+
+    @Override
+    public Iterable<V> values() {
+        return map.values();
+    }
 }
